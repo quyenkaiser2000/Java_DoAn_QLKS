25dac116586219b495716d535b0e198bcae80415
quyenkaiser2000 <nguyentanquyen2000@gmail.com>
2021-04-15 18:06:44 +0700

    Giao dien

---
+++ src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));
+       Parent root1 = FXMLLoader.load(getClass().getResource("fxml/NhanVien.fxml"));
+        primaryStage.setTitle("LOGIN");
+       // primaryStage.setScene(new Scene(root, 1920, 1000));
+        primaryStage.setScene(new Scene(root1, 1920, 1070));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
